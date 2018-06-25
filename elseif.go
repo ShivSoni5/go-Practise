633460c63ad2f77fc9f5275a0d5e2ad43cbae1ea
ShivSoni5 <shivsonic05@gmail.com>
2018-06-26 02:32:23 +0530

    else what if

---
+++ elseif.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 7
+	if n%2 == 0 {
+		fmt.Println(n, "is even")
+	} else {
+		fmt.Println(n, "is odd")
+	}
+
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
+
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+}
